1c077853485e8bbb5fad402389629a15120c390d
Wojciech Jarosz <jaroszw@gmail.com>
2023-12-14 14:15:58 +0100

    initial client side

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import Register from './Register';
+import TextInputWithFocusButton from './useRef';
+
+function App() {
+  return (
+    <main className="App">
+      <Register />
+      <TextInputWithFocusButton />
+    </main>
+  );
+}
+
+export default App;
