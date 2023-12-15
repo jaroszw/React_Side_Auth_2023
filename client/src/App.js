4238a6049d75e1504fdedf156316577460c40422
Wojciech Jarosz <jaroszw@gmail.com>
2023-12-15 12:43:30 +0100

    register componetnt finsihed, axios beeing implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
 import Register from './Register';
-import TextInputWithFocusButton from './useRef';
 
 function App() {
   return (
     <main className="App">
       <Register />
-      <TextInputWithFocusButton />
     </main>
   );
 }
